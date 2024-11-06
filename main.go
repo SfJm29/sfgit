3076e7013fdc720d751b4cbf3025cc8f2dacb877
mar <garbagehlam@gmail.com>
2024-11-06 12:43:13 +0300

    небольшие правки

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	var data string
-	fmt.Print("Введите целое число: ")
+	fmt.Print("Введите данные: ")
 	_, err := fmt.Scanln(&data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %v\n", data)
+	fmt.Printf("Вы ввели: %v\n", data)
 }
